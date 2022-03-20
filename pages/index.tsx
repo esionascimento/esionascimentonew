5b31638536b6c52d7be07781c4fdfb696047a98b
esionascimento <esio.392@protonmail.com>
2022-03-20 17:03:03 -0400

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Esio Nascimento</title>
+      </Head>
+
+      <main>
+        <h1>
+          em desenvolvimento
+        </h1>
+      </main>
+    </div>
+  )
+}
