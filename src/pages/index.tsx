25d5e5cf4c0eeaa8a23c0bcbe999f223bb30973f
esionascimento <esio.392@protonmail.com>
2022-03-24 00:43:35 -0400

    create folder images | create bio - contact information

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,13 @@
-import React from 'react';
+import React from "react";
 import Head from 'next/head';
 import NavigationTop from '../components/navigation';
+import Bio from '../components/bio'
 
-export default function Home() {
-  return (
-    <div>
-      <NavigationTop />
-      <Head>
-        <title>Esio Nascimento</title>
-      </Head>
-
-      <main>
-        <h1>
-          em desenvolvimento
-        </h1>
-      </main>
-    </div>
-  );
+export default function Home()    {
+    return (
+        <div>
+            <NavigationTop />
+            <Bio />
+        </div>
+    );
 }
