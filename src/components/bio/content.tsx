84e961a8feff5a576b73638d46a6e52fd676e193
esionascimento <esio.392@protonmail.com>
2022-03-24 15:03:58 -0400

    refactor bio > content | using Col, Row, to keep responsive

--- src/components/bio/content.tsx
+++ src/components/bio/content.tsx
@@ -1,39 +1,24 @@
-import {
-    Typography,
-} from 'antd';
-import IconLink from './iconLink';
+import { Col } from 'antd';
+import ImgPerfil from '../images/minhafoto-200x200-qualidade-baixa.jpg';
+import * as S from './styles';
 
-const { Paragraph } = Typography;
-
-const Content = (
-    <>
-        <Paragraph>
-            Olá, ...Tenho disposição para encarar novos desafios e desenvolver meus talentos.
-            Como pessoa sou bastante curioso, alegre e criativo tentando compartilhar
-            e retribuir o máximo possível e estou em busca de novos conhecimentos,
-            desafios para desenvolver minhas competências.
-        </Paragraph>
-        <Paragraph>
-            Tenho conhecimentos sobre as seguintes tecnologias:
-            <br />
-            Front End: HTML5, CSS3, Bootstrap, JavaScript, React, Redux.
-            Back End: MongoDB, MySQL, NodeJS, Express.
-        </Paragraph>
-        <div>
-            <IconLink
-                src="https://gw.alipayobjects.com/zos/rmsportal/MjEImQtenlyueSmVEfUD.svg"
-                text="Quick Start"
-            />
-            <IconLink
-                src="https://gw.alipayobjects.com/zos/rmsportal/NbuDUAuBlIApFuDvWiND.svg"
-                text=" Product Info"
-            />
-            <IconLink
-                src="https://gw.alipayobjects.com/zos/rmsportal/ohOEPSYdDTNnyMbGuyLb.svg"
-                text="Product Doc"
-            />
-        </div>
-    </>
-);
+function Content({ children }) {
+    return (
+        <S.RowContent>
+            <Col xs={{ order: 2 }} sm={{ order: 1 }} style={{ flex: 1, padding: '5px' }}>{children}</Col>
+            <S.ColImag
+                xs={{ order: 1 }}
+                sm={{ order: 2 }}
+            >
+                <img
+                    src={ImgPerfil.src}
+                    alt="content"
+                    width="100%"
+                    style={{ borderRadius: '10px' }}
+                />
+            </S.ColImag>
+        </S.RowContent>
+    );
+}
 
 export default Content;
