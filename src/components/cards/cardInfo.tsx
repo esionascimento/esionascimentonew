69de047888c5e5dd2ca6c2c590bbac3ea89a163c
esionascimento <esio.392@protonmail.com>
2022-03-24 14:59:09 -0400

    create file cardInfo | content internal card

---
+++ src/components/cards/cardInfo.tsx
@@ -0,0 +1,32 @@
+import {
+    Card, Col, Tooltip, Divider,
+} from 'antd';
+
+import {
+    DashOutlined,
+} from '@ant-design/icons';
+
+function CardInfo({ title, info, url }) {
+    return (
+        <Col
+            xs={{ span: 16 }}
+            md={{ span: 10 }}
+            lg={{ span: 11 }}
+            style={{ margin: '5px' }}
+        >
+            <Card title={title}>
+                {info}
+                <Divider />
+                <div>
+                    <Tooltip title="Abrir repositorio">
+                        <a href={url}>
+                            <DashOutlined />
+                        </a>
+                    </Tooltip>
+                </div>
+            </Card>
+        </Col>
+    );
+}
+
+export default CardInfo;
