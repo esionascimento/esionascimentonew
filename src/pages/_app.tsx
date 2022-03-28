9a748b6fb6584813993f810d4f185d5ad7d91cf8
esionascimento <esio.392@protonmail.com>
2022-03-28 02:32:57 -0400

    refator index | _app configurations i18n

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { appWithTranslation } from 'next-i18next';
 import { store } from '../store/index';
 
 function MyApp({ Component, pageProps }) {
@@ -10,4 +11,4 @@ function MyApp({ Component, pageProps }) {
     );
 }
 
-export default MyApp;
+export default appWithTranslation(MyApp);
