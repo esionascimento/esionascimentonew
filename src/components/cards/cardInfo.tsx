607773aaf5c83cb9730c10c5a7828c087e90995a
esionascimento <esio.392@protonmail.com>
2022-03-30 19:32:20 -0400

    refactor card, from card with image

--- src/components/cards/cardInfo.tsx
+++ src/components/cards/cardInfo.tsx
@@ -1,31 +1,59 @@
 import {
-    Card, Col, Tooltip, Divider,
+    Card, Col, Tooltip, Divider, Avatar,
 } from 'antd';
-
 import {
+    EditOutlined, EllipsisOutlined, SettingOutlined,
     DashOutlined,
 } from '@ant-design/icons';
 
+const { Meta } = Card;
+
 function CardInfo({ title, info, url }) {
     return (
-        <Col
-            xs={{ span: 16 }}
-            md={{ span: 10 }}
-            lg={{ span: 11 }}
-            style={{ margin: '5px' }}
-        >
-            <Card title={title}>
-                {info}
-                <Divider />
-                <div>
+        <>
+            {/* <Col
+                xs={{ span: 16 }}
+                md={{ span: 10 }}
+                lg={{ span: 11 }}
+                style={{ margin: '5px' }}
+            >
+                <Card title={title}>
+                    {info}
+                    <Divider />
+                    <div>
+                        <Tooltip title="Abrir repositório">
+                            <a href={url}>
+                                <DashOutlined />
+                            </a>
+                        </Tooltip>
+                    </div>
+                </Card>
+            </Col> */}
+            <Card
+                style={{ width: 300, margin: '10px' }}
+                cover={(
+                    <img
+                        alt="example"
+                        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                    />
+                )}
+                actions={[
+                    <SettingOutlined key="setting" />,
+                    <EditOutlined key="edit" />,
                     <Tooltip title="Abrir repositório">
                         <a href={url}>
-                            <DashOutlined />
+                            <EllipsisOutlined key="ellipsis" />
                         </a>
-                    </Tooltip>
-                </div>
+                    </Tooltip>,
+                ]}
+            >
+                <Meta
+                    avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                    title={title}
+                    description="This is the description"
+                />
             </Card>
-        </Col>
+        </>
     );
 }
 
