efae37123d752ff2d9089ddcd8fd88270ff21919
esionascimento <esio.392@protonmail.com>
2022-03-30 23:20:16 -0400

    remove styles pages

--- src/pages/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { Row } from 'antd';
-
-export const RowBody = styled(Row)`
-`;
-
-export const Theme = styled.div`
-    /* background: ${(props) => (props.theme === 'dark' ? '#292929' : '#F0F2F5')}; */
-`;
