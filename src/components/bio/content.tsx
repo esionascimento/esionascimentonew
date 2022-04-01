b843eaf4417f1fbedee5ab00c28a41492ade50db
esionascimento <esio.392@protonmail.com>
2022-03-31 22:47:07 -0400

    refactor content responsivo

--- src/components/bio/content.tsx
+++ src/components/bio/content.tsx
@@ -1,23 +1,32 @@
-import { Col } from 'antd';
+import { Row, Col } from 'antd';
 import ImgPerfil from '../images/minhafoto-200x200-qualidade-baixa.jpg';
+import ContentInfo from './contentInfo';
 import * as S from './styles';
 
-function Content({ children }) {
+function Content() {
     return (
-        <S.RowContent>
-            <Col xs={{ order: 2 }} sm={{ order: 1 }} style={{ flex: 1, padding: '5px' }}>{children}</Col>
+        <Row justify="center">
+            <Col
+                xs={{ span: 24, order: 2 }}
+                md={{ span: 17, order: 1, pull: 1 }}
+            >
+                <ContentInfo />
+            </Col>
+            <Col span={1} />
             <S.ColImag
-                xs={{ order: 1 }}
-                sm={{ order: 2 }}
+                xs={{ span: 24, order: 1 }}
+                md={{ span: 6, order: 2 }}
+                style={{ display: 'flex', justifyContent: 'center' }}
             >
                 <img
                     src={ImgPerfil.src}
                     alt="content"
-                    width="100%"
-                    style={{ borderRadius: '10px' }}
+                    width="175px"
+                    height="175px"
+                    style={{ borderRadius: '30px' }}
                 />
             </S.ColImag>
-        </S.RowContent>
+        </Row>
     );
 }
 
