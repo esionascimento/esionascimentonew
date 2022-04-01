77e456d763a896e33ca25655a222bb0ac8774371
esionascimento <esio.392@protonmail.com>
2022-03-31 22:45:39 -0400

    create file optionImg array

---
+++ src/components/bio/optionImg.tsx
@@ -0,0 +1,9 @@
+export const optionsImg = [
+    {
+        index: 1,
+        src: 'https://img.icons8.com/ios/50/000000/docker.png',
+        alt: 'a',
+    },
+];
+
+export const aux = () => { console.log(''); };
