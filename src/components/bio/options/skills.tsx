6dc85344b24c7035fbb53aed098e8317ffd02180
esionascimento <esio.392@protonmail.com>
2022-04-01 20:35:04 -0400

    create folder option para organizar arquivos jsons

---
+++ src/components/bio/options/skills.tsx
@@ -0,0 +1,72 @@
+export const optionSkills = [
+    {
+        index: 1,
+        skills: 'HTML5',
+    },
+    {
+        index: 2,
+        skills: 'CSS3',
+    },
+    {
+        index: 3,
+        skills: 'Bootstrap',
+    },
+    {
+        index: 4,
+        skills: 'JavaScript',
+    },
+    {
+        index: 5,
+        skills: 'ReactJS',
+    },
+    {
+        index: 6,
+        skills: 'NodeJS',
+    },
+    {
+        index: 7,
+        skills: 'MongoDB',
+    },
+    {
+        index: 8,
+        skills: 'MySQL',
+    },
+    {
+        index: 9,
+        skills: 'NextJS',
+    },
+    {
+        index: 10,
+        skills: 'Express',
+    },
+    {
+        index: 11,
+        skills: 'Docker',
+    },
+    {
+        index: 12,
+        skills: 'Java',
+    },
+    {
+        index: 13,
+        skills: 'C++',
+    },
+    {
+        index: 14,
+        skills: 'TypeScript',
+    },
+    {
+        index: 15,
+        skills: 'PostgreSQL',
+    },
+    {
+        index: 16,
+        skills: 'Redux-ToolKit',
+    },
+    {
+        index: 17,
+        skills: 'Ant-Design',
+    },
+];
+
+export const console = [];
